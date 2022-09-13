8e5cae8eef930cddc7d323d54a151cb7e456b1fb
Omale <omalesamuel98@gmail.com>
2022-09-13 02:26:24 +0000

    Check for password match

---
+++ script.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("form");
+const pw1 = document.getElementById("password")
+const pw2 = document.getElementById("confirmPassword");
+const errorMessage = document.getElementById("pwCheck");
+let isPasswordMatched = null;
+
+function comparePassword(e) {
+    if ((pw1.value !== '' && pw2.value !== pw1.value) && (pw2.value !== '' && pw1.value !== pw2.value)) {
+        errorMessage.textContent = '* Passwords do not match!'
+        errorMessage.classList.add('pwError');
+        isPasswordMatched = false;
+    }
+    if (pw1.value === pw2.value) {
+        errorMessage.textContent = '';
+        errorMessage.classList.remove('pwError');
+        isPasswordMatched = true;
+    }
+
+}
+
+function checkForm(e) {
+    if (!isPasswordMatched) {
+        e.preventDefault();
+        comparePassword();
+    } else {
+        return;
+    }
+}
+
+
+pw1.addEventListener('keyup', comparePassword);
+pw2.addEventListener('keyup', comparePassword);
+form.addEventListener('submit', checkForm);
+
